39284e0265846573c2d59e7cbf9cb78af2197c71
Alasdair Clarke <a.clarke@essex.ac.uk>
2017-11-30 10:53:06 +0000

    Fixed model 1
    Better weak priors (still weak, but more correct now).
    
    Fixed a bug.
    
    tidied up

--- analysis/3_modelling_data.R
+++ analysis/3_modelling_data.R
@@ -1,32 +1,31 @@
 library(rethinking)
-#library(tidyverse)
 
 #### load in the data #### 
 load("scratch/processed_data_nar.rda")
 
 # make the tibble into a dataframe for map 
-test <- as.data.frame(df_correct_only)
+df <- as.data.frame(df_correct_only)
 
-#### first model #### 
-# Just the difficulty and targ_pres
-# no random effects are present in this model
-# in Alasdair's words, it's therefore "crap"
+######################### model 1 ################################
+# This model is a simple test, and clearly wrong!
+# No random effects are present in this model
+# Linear model, does not take skew of reaction times into account
+# None the less, we can use it to check we know what we're doing!
 # rt ~ difficulty * targ_press
+# 
+# Use dumb weak (0, 10) priors
+#################################################################
 
 m_tp_diff_1 <- map(
-  alist(
-    rt ~ dnorm( mu , sigma ) ,
-    mu <- a + b_diff*difficulty + b_tp*targ_pr , 
-    a ~ dnorm( 1 , 10 ) ,
-    b_diff ~ dnorm( 1 , 10 ) , 
-    b_tp ~ dnorm( 1 , 10 ) , 
-    sigma ~ dnorm( 0 , 1 )
-  ) ,
-data = test)
-
-
+	alist(
+	    rt ~ dnorm(mu, sigma),
+	    mu <- a + b_diff*difficulty + b_tp*targ_pr, 
+	    # specify priors!
+	    a ~ dnorm(0, 10),
+	    b_diff ~ dnorm(0, 10), 
+	    b_tp ~ dnorm(0, 10), 
+	    sigma ~ dcauchy(0, 1)),
+	data = df)		
 
 #### save models into a subfolder within scratch ####
 save(m_tp_diff_1, file = "scratch/models/m_tp_diff_1")
-
-
