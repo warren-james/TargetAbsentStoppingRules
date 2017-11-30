726c60db3977500d0fd4634cab0e1a1e5b8f9f8c
Alasdair Clarke <a.clarke@essex.ac.uk>
2017-11-30 13:57:35 +0000

    Working on model plots

--- analysis/4_simple_model_summary.R
+++ analysis/4_simple_model_summary.R
@@ -30,31 +30,51 @@ plt <- plt + scale_x_continuous(limits = c(0, pi))
 plt <- plt + theme_bw()
 plt
 
-# plot range of fits
-
-# for ta, lower line is
+# get 97% credible interval for regression lines
 model_lines <- rbind(
 	data.frame(
-	x = c(0, 1),
-	lower = sapply(c(0,1), 
-		function(x) {y = quantile(post$a + post$b_diff * x, 0.05)}),
-	upper = sapply(c(0,1), 
-		function(x) {y = quantile(post$a + post$b_diff * x, 0.95)}),
-	targ = "absent"),
+		difficulty = c(0, 1),
+		lower = sapply(c(0,1), 
+			function(x) {y = quantile(post$a + post$b_diff * x, 0.015)}),
+		upper = sapply(c(0,1), 
+			function(x) {y = quantile(post$a + post$b_diff * x, 0.985)}),
+		targ_pr = "absent"),
 	data.frame(
-	x = c(0, 1),
-	lower = sapply(c(0,1), 
-		function(x) {y = quantile(post$a + post$b_tp + post$b_diff * x, 0.05)}),
-	upper = sapply(c(0,1), 
-		function(x) {y = quantile(post$a + post$b_tp + post$b_diff * x, 0.95)}),
-	targ = "present"))
+		difficulty = c(0, 1),
+		lower = sapply(c(0,1), 
+			function(x) {y = quantile(post$a + post$b_tp + post$b_diff * x, 0.015)}),
+		upper = sapply(c(0,1), 
+			function(x) {y = quantile(post$a + post$b_tp + post$b_diff * x, 0.985)}),
+		targ_pr = "present")
+	)
+
+# sample a load of points from the model so we can get a feel for what it predicts
+pred_ta = sim(m_tp_diff_1, data = list(difficulty = seq(0,1, 0.1), targ_pr = 0))
+pred_tp = sim(m_tp_diff_1, data = list(difficulty = seq(0,1, 0.1), targ_pr = 1))
+
+# get 97% HDPI
+ta_PI = apply(pred_ta, 2, PI, prob = 0.97)
+tp_PI = apply(pred_tp, 2, PI, prob = 0.97)
+
+pred_lines = data.frame(
+	difficulty = rep(seq(0,1,0.1),2),
+	targ_pr = rep(c("absent", "present"), each = 11),
+	rbind(t(ta_PI), t(tp_PI)))
+names(pred_lines)[3:4] = c("lower", "upper")
 
 
-
-plt <- ggplot(model_lines, aes(x = x, fill = targ))
+plt <-  ggplot()
+plt <- plt + geom_jitter(data = df_correct_only, 
+	aes(x = difficulty, y = rt, colour = as.factor(targ_pr))) 
 # add model fit
-plt <- plt + geom_ribbon(aes(ymin = lower, ymax = upper))
-
+plt <- plt + geom_ribbon(data= model_lines, aes(x = difficulty, ymin = lower, ymax = upper, fill = targ_pr))
+# add prediction range
+plt <- plt + geom_ribbon(data = pred_lines, aes(
+	x = pred_lines$difficulty, 
+	ymin = pred_lines$lower,
+	ymax = pred_lines$upper,
+	fill = pred_lines$targ),
+	alpha = 0.5)
 # spec theme
 plt <- plt + scale_x_continuous("search difficulty", limits = c(0, pi))
 plt <- plt + scale_y_continuous("reaction time", limits = c(0, 10))
