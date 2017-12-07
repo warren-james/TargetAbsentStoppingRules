66617a948bc5fea53b6de9bb1903eff8df2d50f0
Alasdair Clarke <a.clarke@essex.ac.uk>
2017-12-07 10:19:53 +0000

    some simple mixed models!
    random intercept only

--- analysis/3b_mixed_models.R
+++ analysis/3b_mixed_models.R
@@ -10,8 +10,10 @@ df <- as.data.frame(df_correct_only)
 rm(df_correct_only)
 
 
-######################### model 5 ################################
+######################### model 1 ################################
 # Finally add random intercepts!
+# 
+# Start simple with intercepts only!
 # 
 # Use a log-link to account for skew!
 # rt ~ exp(difficulty * targ_press)
@@ -19,17 +21,45 @@ rm(df_correct_only)
 # Using informed priors
 #################################################################
 
-m_tp_diff_5 <- map(
+m_tp_diff_1 <- map2stan(
 	alist(
 	    rt ~ dlnorm(mu, sigma),
-	    mu <- a + b_diff*theta + b_tp*targ_pr + b_tp_diff*targ_pr*theta, 
+	    mu <- a + a_p[participant], 
 	    # specify priors!
 	    a ~ dnorm(1, 1),
+	    a_p[participant] ~ dnorm(0, sig_p),
+	    sigma ~ dcauchy(0, 1),
+	    sig_p ~ dcauchy(0, 1)),
+	data = df)
+
+#### save models into a subfolder within scratch ####
+save(m_tp_diff_1, file = "scratch/models/m_tp_diff_mixed_1")
+
+
+######################### model 2 ################################
+# Finally add random intercepts!
+# 
+# Add in slopes
+# 
+# Use a log-link to account for skew!
+# rt ~ exp(difficulty * targ_press)
+# 
+# Using informed priors
+#################################################################
+
+m_tp_diff_2 <- map2stan(
+	alist(
+	    rt ~ dlnorm(mu, sigma),
+	    mu <- a + a_p[participant] + b_diff*theta + b_tp*targ_pr + b_tp_diff*targ_pr*theta,  
+	    # specify priors!
+	    a ~ dnorm(1, 1),
+	    a_p[participant] ~ dnorm(0, sig_p),
 	    b_diff ~ dnorm(1, 1.5), 
 	    b_tp ~ dnorm(-1, 1),
 	    b_tp_diff ~ dnorm(-1, 1),
-	    sigma ~ dcauchy(0, 1)),
-	data = df, start = start_points)		
+	    sigma ~ dcauchy(0, 1),
+	    sig_p ~ dcauchy(0, 1)),
+	data = df)
 
 #### save models into a subfolder within scratch ####
-save(m_tp_diff_5, file = "scratch/models/m_tp_diff_5")
+save(m_tp_diff_2, file = "scratch/models/m_tp_diff_mixed_2")
