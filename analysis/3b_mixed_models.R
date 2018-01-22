183b8db2def4aafd26f22d12219a14d79878bc53
warren-james <w.james@abdn.ac.uk>
2018-01-22 11:44:28 +0000

    Added to models
    Now there is a centred theta variable, plus a model with weaker priors

--- analysis/3b_mixed_models.R
+++ analysis/3b_mixed_models.R
@@ -63,3 +63,47 @@ m_tp_diff_2 <- map2stan(
 
 #### save models into a subfolder within scratch ####
 save(m_tp_diff_2, file = "scratch/models/m_tp_diff_mixed_2")
+
+
+#### maybe try a weaker prior for the intercept because it moves a lot ####
+# also, should the intercept be allowed to go below 0?
+# At the moment it shouldn't as we have only scaled the difficulty and not
+# centred it... which we should do seeing as we have centred everything else
+
+m_tp_diff_3 <- map2stan(
+  alist(
+    rt ~ dlnorm(mu, sigma),
+    mu <- a + a_p[participant] + b_diff*theta + b_tp*targ_pr + b_tp_diff*targ_pr*theta,  
+    # specify priors!
+    a ~ dnorm(1, 10),
+    a_p[participant] ~ dnorm(0, sig_p),
+    b_diff ~ dnorm(1, 1.5), 
+    b_tp ~ dnorm(-1, 1),
+    b_tp_diff ~ dnorm(-1, 1),
+    sigma ~ dcauchy(0, 1),
+    sig_p ~ dcauchy(0, 10)),
+  data = df)
+
+#### save models into a subfolder within scratch ####
+save(m_tp_diff_3, file = "scratch/models/m_tp_diff_mixed_3")
+
+
+#### tre the same again using centred diff data ####
+m_tp_diff_4 <- map2stan(
+  alist(
+    rt ~ dlnorm(mu, sigma),
+    mu <- a + a_p[participant] + b_diff*theta_c + b_tp*targ_pr + b_tp_diff*targ_pr*theta_c,  
+    # specify priors!
+    a ~ dnorm(1, 10),
+    a_p[participant] ~ dnorm(0, sig_p),
+    b_diff ~ dnorm(1, 3), 
+    b_tp ~ dnorm(-1, 3),
+    b_tp_diff ~ dnorm(-1, 3),
+    sigma ~ dcauchy(0, 10),
+    sig_p ~ dcauchy(0, 10)),
+  data = df)
+
+#### save models into a subfolder within scratch ####
+save(m_tp_diff_4, file = "scratch/models/m_tp_diff_mixed_4")
+
+
