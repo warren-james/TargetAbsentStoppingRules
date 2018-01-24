04f9a2ec07f11c896adf7da595f306443d411629
warren-james <w.james@abdn.ac.uk>
2018-01-24 20:02:43 +0000

    6_ plot function changes

--- analysis/65a_TA_only_models_summary.R
+++ analysis/65a_TA_only_models_summary.R
@@ -12,8 +12,8 @@ get_hpdi_region_from_samples <- function(m, post, ln = TRUE) {
   
   pred_data <- list(
     participant = rep(1:length(unique(df$participant)), each = 4),
-    theta =   rep(c(0.12, 0.88, 0.12, 0.88), length(unique(df$participant))),
-    targ_pr = rep(c(0, 0, 1, 1), length(unique(df$participant))))
+    theta =   rep(c(0.12, 0.88, 0.12, 0.88), length(unique(df$participant))))#,
+    #targ_pr = rep(c(0, 0, 1, 1), length(unique(df$participant))))
 
   mu <- link(m, data = pred_data)
   mu.PI <- apply(mu, 2, PI)
@@ -21,8 +21,8 @@ get_hpdi_region_from_samples <- function(m, post, ln = TRUE) {
   pred_data$lower <- mu.PI[1,]
   pred_data$upper <- mu.PI[2,]
 
-  pred_data$targ_pr <- as.factor(pred_data$targ_pr)
-  levels(pred_data$targ_pr) <- c("absent", "present")
+  #pred_data$targ_pr <- as.factor(pred_data$targ_pr)
+  #levels(pred_data$targ_pr) <- c("absent", "present")
 
   if (ln == TRUE) { 
     pred_data$lower <- exp(pred_data$lower)
@@ -40,7 +40,7 @@ plot_model_mixed_facet <- function(pred_lines, model_lines, title_text, lt) {
   plt <-  ggplot()  
   # add model fit
   plt <- plt + geom_ribbon(data = model_lines, 
-    aes(x = theta, ymin = lower, ymax = upper, fill = targ_pr))
+    aes(x = theta, ymin = lower, ymax = upper))#, fill = targ_pr))
   # add empirical data points
   plt <- plt + geom_jitter(data = df, 
     aes(x = theta, y = rt),
@@ -49,7 +49,7 @@ plot_model_mixed_facet <- function(pred_lines, model_lines, title_text, lt) {
   plt <- plt + scale_x_continuous("search difficulty", 
     limits = c(0, 1), expand = c(0, 0))
   plt <- plt + scale_y_continuous(name = "reaction time", trans = log2_trans(), limits = c(0.5, 32))
-  plt <- plt + scale_fill_discrete(name = "target present")
+  #plt <- plt + scale_fill_discrete(name = "target present")
   plt <- plt + ggtitle(title_text)
   plt <- plt + theme_bw()
   plt <- plt + facet_wrap( ~ participant)
@@ -95,3 +95,39 @@ model_lines <- get_hpdi_region_from_samples(m_ta_only_1, post, TRUE)
 
 
 plot_model_mixed_facet(pred_lines, model_lines, "random intercepts", TRUE)
+
+
+#################################################################
+# model 2 - getting started
+#################################################################
+
+load("scratch/models/m_ta_only_1")
+precis(m_ta_only_2)
+
+# extract samples from model
+post <- extract.samples(m_ta_only_2)
+
+# get 95% HPDI for each person 
+#  this is a horrible hack, what's a nicer way of doing this?
+intercepts_p <- as.tibble(t(as.tibble(lapply(as.data.frame(post$a_p), quantile, probs = c(0.025, 0.975)))))
+names(intercepts_p) = c("lower", "upper")
+intercepts_p$participant = 1:nrow(intercepts_p)
+# this is a little incorrect... should do everything with samples, then estimate mean, ec
+# will fix later!
+intercepts_p$lower <- exp(intercepts_p$lower + mean(post$a))
+intercepts_p$upper <- exp(intercepts_p$upper + mean(post$a))
+
+
+# display range of intercepts, again this is probably not 100% correct 
+# (but good enough for now?)
+theta <- mean(post$sig_p)
+mu <- mean(post$a)
+participant_range <- c(
+  qlnorm(mu, theta, 0.025), qlnorm(mu, theta, 0.975))
+
+model_lines <- get_hpdi_region_from_samples(m_ta_only_2, post, TRUE)
+
+
+plot_model_mixed_facet(pred_lines, model_lines, "random intercepts", TRUE)
+
+
