1e350a07822a31c9470126c9f0309de34ce340cc
Alasdair Clarke <a.clarke@essex.ac.uk>
2018-01-24 14:19:54 +0000

    Random slopes are working!

--- analysis/5a_TA_only_models.R
+++ analysis/5a_TA_only_models.R
@@ -39,18 +39,41 @@ save(m_ta_only_1, file = "scratch/models/m_ta_only_1")
 m_ta_only_2 <- map2stan(
   alist(
     rt ~ dlnorm(mu, sigma), 
-    mu <- a + a_p[participant] + b_theta_c*theta_c, 
+    mu <- a + a_p[participant] + b_theta*theta, 
     # priors
     a ~ dnorm(1, 3),
     a_p[participant] ~ dnorm(0, sig_p),
-    b_theta_c ~ dnorm(1,3)
+    b_theta ~ dnorm(1,3),
     sigma ~ dcauchy(0, 3),
     sig_p ~ dcauchy(0, 3)
   ),
   data = df)
 
 # save the model 
-save(m_ta_only_2, file = "scratch/models/m_ta_only_1")
+save(m_ta_only_2, file = "scratch/models/m_ta_only_2")
 
+########## Model 3 #########
+# Add in random slopes for Theta #
+# using similar notation to 13.22 in McElreath
+############################
 
+m_ta_only_3 <- map2stan(
+  alist(
+    rt ~ dlnorm(mu, sigma), 
+    mu <-  a + a_p[participant] + (b_theta + b_theta_p[participant]) * theta, 
 
+    # # adaptive priors
+    c(a_p, b_theta_p)[participant] ~ dmvnormNC(sigma_p, Rho),
+
+    # fixed priors  
+    a ~ dnorm(1, 3),
+    b_theta ~ dnorm(1,3),
+    sigma ~ dcauchy(0, 3),
+    sigma_p ~ dcauchy(0, 3),
+    Rho ~ dlkjcorr(2)
+
+  ),
+  data = df)
+
+# save the model 
+save(m_ta_only_3, file = "scratch/models/m_ta_only_3")
