d72fae4f30f2be5bfd6a9ab6f19b96f380fce95b
warren-james <w.james@abdn.ac.uk>
2018-01-24 12:54:18 +0000

    Added model with slope to 5_TA

--- analysis/5a_TA_only_models.R
+++ analysis/5a_TA_only_models.R
@@ -32,7 +32,25 @@ data = df)
 save(m_ta_only_1, file = "scratch/models/m_ta_only_1")
 
 
+########## Model 2 #########
+# Add in a slope for Theta #
+############################
 
+m_ta_only_2 <- map2stan(
+  alist(
+    rt ~ dlnorm(mu, sigma), 
+    mu <- a + a_p[participant] + b_theta_c*theta_c, 
+    # priors
+    a ~ dnorm(1, 3),
+    a_p[participant] ~ dnorm(0, sig_p),
+    b_theta_c ~ dnorm(1,3)
+    sigma ~ dcauchy(0, 3),
+    sig_p ~ dcauchy(0, 3)
+  ),
+  data = df)
+
+# save the model 
+save(m_ta_only_2, file = "scratch/models/m_ta_only_1")
 
 
 
