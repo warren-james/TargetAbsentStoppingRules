29ae778508ed3baeb94ac9c97ad38c172dbba1a3
warren-james <w.james@abdn.ac.uk>
2018-02-02 16:15:30 +0000

    Added models to 5a_
    Need to run the last model to check it works. Looks like the most "correct" one for what we want the modelling to do

--- analysis/5a_TA_only_models.R
+++ analysis/5a_TA_only_models.R
@@ -87,10 +87,13 @@ m_ta_only_3 <- map2stan(
 # save the model 
 save(m_ta_only_3, file = "scratch/models/m_ta_only_3")
 
+##### Attempts to add in block_type ####
 
 ##### temp #########
 # with fixed effects for block_type
 # I think that's what it is anyway, but I could be wrong
+# This uses dummy variable coding and seems to work, so we might
+# want to stick with this way of doing things
 
 m_ta_only_temp <- map2stan(
   alist(
@@ -125,8 +128,10 @@ save(m_ta_only_temp, file = "scratch/models/m_ta_only_temp")
 
 #### same as above but instead using coerce_index function ####
 # model rerun, but I don't think it's right...
-#make block_type_id 
+# make block_type_id 
 # df$block_type_id <- coerce_index(df$block_type)
+# "a" doesn't really mean anything in this model... 
+# so we should probably ignore it 
 
 # run model
 m_ta_only_temp_2 <- map2stan(
@@ -153,4 +158,64 @@ m_ta_only_temp_2 <- map2stan(
 
 # save the file 
 save(m_ta_only_temp_2, file = "scratch/models/m_ta_only_temp_2")
+
+
+#### add in varying slopes for block_type ####
+# This one probably won't work, it's using the index variable coding
+# but that means that "a" becomes a bit meaningless
+
+m_ta_only_temp_3 <- map2stan(
+  alist(
+    rt ~ dlnorm(mu, sigma), 
+    mu <-  A + B_theta,
+    A <- a + a_p[participant] + a_bt[block_type_id],
+    B_theta <- (b_theta + b_theta_p[participant] + b_theta_bt[block_type_id]) * theta, 
+    
+    # adaptive priors
+    c(a_p, b_theta_p)[participant] ~ dmvnormNC(sigma_p, Rho_p),
+    c(a_bt, b_theta_bt)[block_type_id] ~ dmvnormNC(sigma_bt, Rho_bt),
+    
+    # fixed priors  
+    a ~ dnorm(1, 2),
+    b_theta ~ dnorm(1,3),
+    sigma ~ dcauchy(0, 1.5),
+    sigma_p ~ dcauchy(0, 1.5),
+    sigma_bt ~ dcauchy(0,1.5),
+    Rho_p ~ dlkjcorr(4),
+    Rho_bt ~ dlkjcorr(4)
+  ),
+  data = df,
+  iter = 2000, warmup = 1000, chains = 3, cores = 3)
+
+
+#### using dummy variables #### 
+# This model has intercepts and slopes for block type
+# These effects aren't by participant though
+
+m_ta_only_temp_4 <- map2stan(
+  alist(
+    rt ~ dlnorm(mu, sigma), 
+    mu <-  A  + B_theta + B_blk + B_blk_th,
+    A <- a + a_p[participant],
+    B_theta <- (b_theta + b_theta_p[participant]) * theta, 
+    B_blk <- b_issi*issi + b_isra*isra,
+    B_blk_th <- (b_si_theta*issi + b_ra_theta*isra) * theta,
+    
+    # # adaptive priors
+    c(a_p, b_theta_p)[participant] ~ dmvnormNC(sigma_p, Rho),
+    
+    # fixed priors  
+    a ~ dnorm(1, 3),
+    b_theta ~ dnorm(1,3),
+    b_isra ~ dnorm(1,3),
+    b_issi ~ dnorm(1,3),
+    b_ra_theta ~ dnorm(1,3),
+    b_si_theta ~ dnorm(1,3),
+    sigma ~ dcauchy(0, 1),
+    sigma_p ~ dcauchy(0, 1),
+    Rho ~ dlkjcorr(4)
+  ),
+  data = df,
+  iter = 2000, warmup = 1000, chains = 3, cores = 3)
+
 
