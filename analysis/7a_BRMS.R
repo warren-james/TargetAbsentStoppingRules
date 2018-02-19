f5d7479070506177a3c7b5fd44045304cdc2395d
warren-james <w.james@abdn.ac.uk>
2018-02-19 17:58:52 +0000

    Added priors
    I think this is how you set the priors, this model should run a bit better now that it's not using very wide priors

--- analysis/7a_BRMS.R
+++ analysis/7a_BRMS.R
@@ -20,7 +20,7 @@ rm(df_TA)
 ##### NB: retreiving priors ######
 # if you want to see what priors # 
 # can be set in your model, use: #
-### get_priors(model_formula)  ###
+### get_priors(model formula)  ###
 
 ########## Model 1 ########## 
 # Just RT by theta for now  #
@@ -31,8 +31,9 @@ rm(df_TA)
 
 m1_rt_theta <- brm(rt ~ theta + (1 + theta|participant),
                    data = df, family = lognormal,
-                   prior = c(set_prior("normal(0.55,1)", class = "b", coef = "theta"),
-                             set_prior("normal(0.55,1", class = "Intercept")), # think this sets intercept?
+                   prior = c(set_prior("normal(1,3)", class = "b", coef = "theta"),
+                             set_prior("normal(0.55,1)", class = "Intercept"),# think this sets intercept?
+                             set_prior("cauchy(0,1.5)", class = "sd")), 
                    warmup = 1000, iter = 2000, chains = 4)
 
 save(m1_rt_theta, file = "scratch/models/brm_m1")
